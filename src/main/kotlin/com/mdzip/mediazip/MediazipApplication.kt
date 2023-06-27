5d3f3be4e0d7bf7540fbe0a88014bc031aa07f39
artelee <eunhye4611@naver.com>
2023-06-27 23:49:18 +0900

    [init] create project

---
+++ src/main/kotlin/com/mdzip/mediazip/MediazipApplication.kt
@@ -0,0 +1,11 @@
+package com.mdzip.mediazip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MediazipApplication
+
+fun main(args: Array<String>) {
+    runApplication<MediazipApplication>(*args)
+}
---
+++ src/test/kotlin/com/mdzip/mediazip/MediazipApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mdzip.mediazip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MediazipApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
